f164e0b2ec7be211e951e0c91292b060a3d03f13
j-bill <julian.billinger+1@gmail.com>
2020-05-14 11:40:26 +0200

    check_online fixed, removed some never used files

--- files/check_online.sh
+++ files/check_online.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ping -c 2 google.com > /dev/tty
+ping -c 2 google.com > /dev/null
 if [ $? -ne 0 ]; then
   systemctl restart openvpn
+  echo "<p> `date` Connection Lost</p>" >> /var/www/html/vnstati/index.html
 fi
